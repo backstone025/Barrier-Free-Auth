321785fc0caa71944af6d1185f8c85236c629280
backstone025 <112708588+backstone025@users.noreply.github.com>
2026-01-01 19:27:08 +0900

    환경 세팅

---
+++ barrier-free-auth-application/src/main/java/com/BaeBrother/barrier_free_auth_application/barrier_free_auth_application/BarrierFreeAuthApplication.java
@@ -0,0 +1,13 @@
+package com.BaeBrother.barrier_free_auth_application.barrier_free_auth_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarrierFreeAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarrierFreeAuthApplication.class, args);
+	}
+
+}
---
+++ barrier-free-auth-application/src/test/java/com/BaeBrother/barrier_free_auth_application/barrier_free_auth_application/BarrierFreeAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BaeBrother.barrier_free_auth_application.barrier_free_auth_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarrierFreeAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
