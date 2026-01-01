6cf652c69b8893da98cf461797029178fb31134c
jintronix03 <252435370+jintronix03@users.noreply.github.com>
2026-01-01 20:31:26 +0900

    test입니다.

---
+++ barrier-free-auth-application/src/main/java/com/BaeBrother/barrier_free_auth_application/barrier_free_auth_application/shopping/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.BaeBrother.barrier_free_auth_application.barrier_free_auth_application.shopping;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
